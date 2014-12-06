349777eb1ee5dac39289acd3ce015dd22df88e66
九十 <wongguang.wg@alibaba-inc.com>
2014-12-07 01:46:32 +0800

    找找感觉

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 module.exports = entry;
 module.exports.SimpleLexer = require("./lib/simple_lexer");
-module.exports.LR0 = require("./lib/lr0");
 module.exports.LR1 = require("./lib/lr1");
 module.exports.SLR1 = require("./lib/slr1");
 module.exports.LALR1 = require("./lib/lalr1");
