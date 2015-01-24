e770bd5c13513b8fe2d7cd2576025bb954a1eb8f
九十 <wongguang.wg@alibaba-inc.com>
2015-01-24 18:19:42 +0800

    tmp

--- demo/simple.js
+++ demo/simple.js
@@ -6,6 +6,7 @@ var generatorOfTheSimple = new Aeropress.SLR1({
     ignore : SimpleCFG.ignore,
     lex : SimpleCFG.lex
 });
+console.log( generatorOfTheSimple.getTable() );
 
 //var syntaxParserCodeCore = generatorOfTheSimple.genCodeCore();
 //codeCore.writeToFS(__dirname + "/simple_core_code.js");
